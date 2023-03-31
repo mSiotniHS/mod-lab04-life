f4f00a1502adbe66c6f5594312f8974f4970cca9
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-31 09:12:22 +0300

    Разложил классы по файлам

---
+++ Life/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Life
+{
+	public class Cell
+	{
+		public bool IsAlive;
+		public readonly List<Cell> neighbors = new List<Cell>();
+		private bool IsAliveNext;
+		public void DetermineNextLiveState()
+		{
+			int liveNeighbors = neighbors.Where(x => x.IsAlive).Count();
+			if (IsAlive)
+				IsAliveNext = liveNeighbors == 2 || liveNeighbors == 3;
+			else
+				IsAliveNext = liveNeighbors == 3;
+		}
+		public void Advance()
+		{
+			IsAlive = IsAliveNext;
+		}
+	}
+}
