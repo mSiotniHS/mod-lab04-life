3e96eeab2ca091ee6ffa7d55825b12222c131e40
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-01 12:26:45 +0300

    [Life/feat] Создал serializable структуру `BoardSettings`

---
+++ Life/BoardSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Life
+{
+	[Serializable]
+	public struct BoardSettings
+	{
+		public uint Width { get; set; }
+		public uint Height { get; set; }
+		public uint CellSize { get; set; }
+		public double LiveDensity { get; set; }
+	}
+}
