32caec74827448d475cdf37e3ff016d1bda63059
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-01 12:28:00 +0300

    [Life/refac] Вынес из `Board` логику формирования `Cells` в `BoardBuilder`

--- Life/Board.cs
+++ Life/Board.cs
@@ -1,40 +1,17 @@
-﻿using System;
-
-namespace Life
+﻿namespace Life
 {
 	public class Board
 	{
 		public readonly Cell[,] Cells;
-		public readonly int CellSize;
+		public readonly uint CellSize;
 
 		public int Columns => Cells.GetLength(0);
 		public int Rows => Cells.GetLength(1);
-		public int Width => Columns * CellSize;
-		public int Height => Rows * CellSize;
 
-		public Board(int width, int height, int cellSize, double liveDensity = .1)
+		public Board(Cell[,] cells, uint cellSize)
 		{
+			Cells = cells; // WARNING: копировать?
 			CellSize = cellSize;
-
-			Cells = new Cell[width / cellSize, height / cellSize];
-			for (var x = 0; x < Columns; x++)
-			{
-				for (var y = 0; y < Rows; y++)
-				{
-					Cells[x, y] = new Cell();
-				}
-			}
-
-			ConnectNeighbors();
-			Randomize(liveDensity);
-		}
-
-		private readonly Random _rand = new Random();
-
-		private void Randomize(double liveDensity)
-		{
-			foreach (var cell in Cells)
-				cell.IsAlive = _rand.NextDouble() < liveDensity;
 		}
 
 		public void Advance()
@@ -43,30 +20,6 @@ namespace Life
 				cell.DetermineNextLiveState();
 			foreach (var cell in Cells)
 				cell.Advance();
-		}
-
-		private void ConnectNeighbors()
-		{
-			for (var x = 0; x < Columns; x++)
-			{
-				for (var y = 0; y < Rows; y++)
-				{
-					var xL = (x > 0) ? x - 1 : Columns - 1;
-					var xR = (x < Columns - 1) ? x + 1 : 0;
-
-					var yT = (y > 0) ? y - 1 : Rows - 1;
-					var yB = (y < Rows - 1) ? y + 1 : 0;
-
-					Cells[x, y].Neighbors.Add(Cells[xL, yT]);
-					Cells[x, y].Neighbors.Add(Cells[x, yT]);
-					Cells[x, y].Neighbors.Add(Cells[xR, yT]);
-					Cells[x, y].Neighbors.Add(Cells[xL, y]);
-					Cells[x, y].Neighbors.Add(Cells[xR, y]);
-					Cells[x, y].Neighbors.Add(Cells[xL, yB]);
-					Cells[x, y].Neighbors.Add(Cells[x, yB]);
-					Cells[x, y].Neighbors.Add(Cells[xR, yB]);
-				}
-			}
 		}
 	}
 }
---
+++ Life/BoardBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Life
+{
+	public static class BoardBuilder
+	{
+		public static Board Build(BoardSettings settings)
+		{
+			var columns = settings.Width / settings.CellSize;
+			var rows = settings.Height / settings.CellSize;
+
+			var cells = new Cell[columns, rows];
+
+			for (var x = 0; x < columns; x++)
+			{
+				for (var y = 0; y < rows; y++)
+				{
+					cells[x, y] = new Cell();
+				}
+			}
+
+			ConnectNeighbors(cells, (int) columns, (int) rows);
+			Randomize(cells, settings.LiveDensity);
+
+			return new Board(cells, settings.CellSize);
+		}
+
+		private static readonly Random Random = new Random();
+
+		private static void Randomize(Cell[,] cells, double liveDensity)
+		{
+			foreach (var cell in cells)
+				cell.IsAlive = Random.NextDouble() < liveDensity;
+		}
+
+		private static void ConnectNeighbors(Cell[,] cells, int columns, int rows)
+		{
+			for (var x = 0; x < columns; x++)
+			{
+				for (var y = 0; y < rows; y++)
+				{
+					var xL = (x > 0) ? x - 1 : columns - 1;
+					var xR = (x < columns - 1) ? x + 1 : 0;
+
+					var yT = (y > 0) ? y - 1 : rows - 1;
+					var yB = (y < rows - 1) ? y + 1 : 0;
+
+					cells[x, y].Neighbors.Add(cells[xL, yT]);
+					cells[x, y].Neighbors.Add(cells[x, yT]);
+					cells[x, y].Neighbors.Add(cells[xR, yT]);
+					cells[x, y].Neighbors.Add(cells[xL, y]);
+					cells[x, y].Neighbors.Add(cells[xR, y]);
+					cells[x, y].Neighbors.Add(cells[xL, yB]);
+					cells[x, y].Neighbors.Add(cells[x, yB]);
+					cells[x, y].Neighbors.Add(cells[xR, yB]);
+				}
+			}
+		}
+	}
+}
