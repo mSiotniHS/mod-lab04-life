88a7420fb98493044e45d4a9501444c36db25d19
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-01 12:04:29 +0300

    Вынес исполняемую программу в отдельный проект

---
+++ App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Life;
+
+namespace App
+{
+	internal static class Program
+	{
+		private static Board _board;
+
+		private static void Reset()
+		{
+			_board = new Board(
+				width: 50,
+				height: 20,
+				cellSize: 1,
+				liveDensity: 0.5);
+		}
+
+		private static void Render()
+		{
+			for (var row = 0; row < _board.Rows; row++)
+			{
+				for (var col = 0; col < _board.Columns; col++)
+				{
+					var cell = _board.Cells[col, row];
+					Console.Write(cell.IsAlive ? '█' : ' ');
+				}
+				Console.Write('\n');
+			}
+		}
+
+		private static void Main()
+		{
+			Reset();
+			while (true)
+			{
+				Console.Clear();
+				Render();
+				_board.Advance();
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
