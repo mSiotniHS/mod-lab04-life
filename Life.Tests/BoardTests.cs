662b25c89efc91cc1124d082b9a2ac19a7cd23da
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-07 13:37:43 +0300

    [Life.Tests/feat] Начал писать тесты для `Board`

---
+++ Life.Tests/BoardTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace Life.Tests
+{
+	public abstract class BoardTests
+	{
+		private static readonly BoardSettings Settings = new BoardSettings
+		{
+			Width = 25,
+			Height = 25,
+			CellSize = 1
+		};
+
+		public class AddFragment
+		{
+			[Fact]
+			public void AddsFragmentCorrectly()
+			{
+				var board = new Board(Settings);
+				var fragment = Fragment.FromString("010\n100\n011");
+
+				board.AddFragment(fragment);
+
+				for (var row = 0; row < fragment.Rows; row++)
+				{
+					for (var column = 0; column < fragment.Columns; column++)
+					{
+						Assert.Equal(fragment.Matrix[row][column], board.Cells[row, column].IsAlive);
+					}
+				}
+			}
+		}
+
+		public class ToFragment
+		{
+			[Fact]
+			public void GeneratesCorrectFragment()
+			{
+				var board = new Board(new [,]
+				{
+					{ new Cell {IsAlive = true} , new Cell {IsAlive = false} },
+					{ new Cell {IsAlive = false}, new Cell {IsAlive = true}  }
+				}, 1);
+
+				var expected = Fragment.FromString("10\n01");
+				var actual = board.ToFragment();
+
+				Assert.Equal(expected.Matrix, actual.Matrix);
+			}
+		}
+	}
+}
