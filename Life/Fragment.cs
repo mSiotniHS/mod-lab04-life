d74ffa4f10edb95f8e2c5dd70849f2ea0f876aa9
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-07 13:35:24 +0300

    [Life/feat] Добавил класс `Fragment`
    Представляет собой сериализируемую часть доски

---
+++ Life/Fragment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Life
+{
+	public sealed class Fragment
+	{
+		public bool[][] Matrix { get; }
+
+		public int Rows => Matrix.Length;
+		public int Columns => Matrix[0].Length;
+
+		public Fragment(bool[][] matrix)
+		{
+			Matrix = matrix;
+		}
+
+		public static Fragment FromString(string raw)
+		{
+			var matrix = raw
+				.Split('\n')
+				.Select(row => row
+					.ToCharArray()
+					.Select(item => item switch
+					{
+						'0' => false,
+						'1' => true,
+						_ => throw new ArgumentOutOfRangeException(nameof(item), item, null)
+					})
+					.ToArray())
+				.ToArray();
+
+			return new Fragment(matrix);
+		}
+
+		public override string ToString()
+		{
+			var builder = new StringBuilder();
+
+			for (var row = 0; row < Rows; row++)
+			{
+				for (var column = 0; column < Columns; column++)
+				{
+					builder.Append(Matrix[row][column] ? '1' : '0');
+				}
+
+				if (row != Rows - 1) builder.Append('\n');
+			}
+
+			return builder.ToString();
+		}
+	}
+}
